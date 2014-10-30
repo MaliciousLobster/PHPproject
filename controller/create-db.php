2c64ba253e93904c18213af0fab334620975b341
NateM <Mr.Awsmness@gmail.com>
2014-10-30 11:12:27 -0700

    video 15

--- controller/create-db.php
+++ controller/create-db.php
@@ -24,12 +24,15 @@
 	$query = $connection->query("CREATE TABLE posts (" //creates a table called post with an id
 		. "id int(11) NOT NULL AUTO_INCREMENT,"        //and a title with up to 255 characters
 		. "title varchar(255) NOT NULL,"               //non of them can be null.
-		. "post text NOT NULL"
+		. "post text NOT NULL,"
 		. "PRIMARY KEY (id) )");						//tells our table that the primary key is my the ID
 
 	if($query) {
-		echo "Succesfully created table: posts";
+		echo " Succesfully created table: posts"; //runs the query, creates the table once
 	}
-	
+	else {
+		echo "<p> $connection->error </p>"; //echos out that the table has already been created
+	}
+
 
 	$connection->close();
