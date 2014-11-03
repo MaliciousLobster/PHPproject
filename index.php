60aaa26884443d4f3bcb7a35966629375980fc70
NateM <Mr.Awsmness@gmail.com>
2014-11-03 11:53:45 -0800

    video 16

--- index.php
+++ index.php
@@ -1,10 +1,5 @@
-<!DOCTYPE html>
-<html>
-	<head>
-	</head>
-	<body>
-		<?php
-	 		require_once(__DIR__ . "/controller/create-db.php"); //takes all the code from create-db.php and puts it here
-	 	?>
-	</body>
-d</html>
+<?php
+	require_once(__DIR__ . "/view/header.php");
+	require_once(__DIR__ . "/view/footer.php");
+	require_once(__DIR__ . "/controller/create-db.php"); //takes all the code from create-db.php and puts it here
+?>
