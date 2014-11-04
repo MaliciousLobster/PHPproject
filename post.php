012107635d46d06a38ff2f994f64ca9c1c645283
NateM <Mr.Awsmness@gmail.com>
2014-11-04 11:08:55 -0800

    videos 19 and 20

--- post.php
+++ post.php
@@ -1,1 +1,4 @@
 <?php
+	require_once(__DIR__ . "/view/header.php"); //takes code from header.php
+	require_once(__DIR__ . "/view/form.php"); //takes code from post.php
+	require_once(__DIR__ . "/view/footer.php"); //takes code from footer.php
---
+++ view/form.php
@@ -0,0 +1,15 @@
+<h1>Create Blog Post</h1>
+
+<form>
+	<div>
+		<label for="title">Title: </label> <!-- inputs text for 'Title' in the table -->
+		<input type="text" name="title"/> <!-- makes the input a text type -->
+	</div>
+	<div>
+		<label for "post">Post: </label> <!-- inputs text for 'Post' in the table -->
+		<textarea name="post"></textarea> <!-- creates a text area -->
+	</div>
+	<div>
+		<button type="submit">Submit</button> <!-- basically refreshes page for now -->
+	</div>
+</form>
