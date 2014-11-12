312d26b2e2f51dbee4f4716613f8ac6880ff2c75
NateM <Mr.Awsmness@gmail.com>
2014-11-12 10:39:11 -0800

    finished video 23: establishing connection to database

--- controller/create-post.php
+++ controller/create-post.php
@@ -1,6 +1,13 @@
 <?php
+	require_once(__DIR__ . "/../model/database.php"); //takes code form database.php
+
+	$connection = new msqli($host, $username, $password, $database); //creates a connection to the database
+
 	$title = filter_input(INPUT_POST, "title", FILTER_SANITIZE_STRING); //filters input recieved from a post that is being submitted to the file. Makes sure that it's a string.
 	$post = filter_input(INPUT_POST, "post", FILTER_SANITIZE_STRING); //filters input recieved from a post that is being submitted to the file. Makes sure that it's a string.
 
 	echo "<p>Title: $title</p>"; //echos input
 	echo "<p>Post: $post</p>"; //echos input
+
+	$connection->close(); //closes the connection
+
--- view/form.php
+++ view/form.php
@@ -1,6 +1,10 @@
+<?php
+	require_once(__DIR__ . "/../model/config.php");
+?>
+
 <h1>Create Blog Post</h1>
 
-<form>
+<form method="post" action="<?php echo $path ."controller/create-post.php"; ?>"> <!-- links post.php to form.php -->
 	<div>
 		<label for="title">Title: </label> <!-- inputs text for 'Title' in the table -->
 		<input type="text" name="title"/> <!-- makes the input a text type -->
