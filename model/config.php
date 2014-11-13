da2a868cb3c532c04ca64e2af5b0e3b0b54b140e
NateM <Mr.Awsmness@gmail.com>
2014-11-13 11:11:15 -0800

    video 26: create a database class

--- model/config.php
+++ model/config.php
@@ -5,3 +5,5 @@
 	$username = "root";
 	$password = "root";
 	$database = "blog_db";
+
+	
---
+++ model/database.php
@@ -0,0 +1,8 @@
+<?php
+	class Database { //these variables are private, which means they cannot be modified outside of this file.
+		private $connection; 
+		private $host;
+		private $username;
+		private $password;
+		private $database;
+	}
