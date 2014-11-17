c4bd931716000c4055f4448bc8f95aef4425ed63
NateM <Mr.Awsmness@gmail.com>
2014-11-17 12:12:05 -0800

    video 29 & 30: putting in the OpenConnection and CloseConnection

--- model/database.php
+++ model/database.php
@@ -14,9 +14,16 @@
 
 		}
 		public function openConnection() {
+			$this->connection = new mysqli($this->host, $this->username, $this->password, $this->database);
 
+			if($this->connection->connect_error){			//accessing a property called 'connect_error', checking for a connection error
+				die("error:" . $connection->connect_error);		//if there's an error it will kill the program & tell the user what the error was
+			}
 		}
 		public function closeConnection() {
+			if(isset($this->connection)){ //checks whether or not there is information inside of the variable.
+				$this->connection->close();
+			}
 
 		}
 		public function query($string) {
