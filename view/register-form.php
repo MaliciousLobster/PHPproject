b76da6d3875f5b5751814e4e7838939bcb26646f
NateM <Mr.Awsmness@gmail.com>
2014-12-02 11:00:26 -0800

    video 37: making the register form

---
+++ view/register-form.php
@@ -0,0 +1,20 @@
+<form> <!-- a place for a user to register -->
+	<div>
+		<label for="email">Email: </label>
+		<input type="text" name="email" />
+	</div>
+
+	<div>
+		<label for="username">Username: </label>
+		<input type="text" name="username" />
+	</div>
+
+	<div>
+		<label for="password">Password: </label>
+		<input type="password" name="password" />
+	</div>
+
+	<div>
+		<button type="submit" class="btn btn-primary" data-toggle="button" aria-pressed="false" autocomplete="off"> Submit </button> <!-- submits the information on the page -->
+	</div>
+</form>
