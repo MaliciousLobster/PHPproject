4d72ad58d8c1732f1c03295d7b6df93b503087a2
NateM <Mr.Awsmness@gmail.com>
2014-12-09 11:11:20 -0800

    video 46: created a login form for the user

---
+++ view/login-form.php
@@ -0,0 +1,22 @@
+<?php
+	require_once(__DIR__ . "/../model/config.php");
+	require_once(__DIR__ . "/header.php");
+?>
+
+<h1>Login</h1>
+
+<form method="post" action="<?php echo $path . "controller/login-user.php"?>">
+	<div>
+		<label for="username">Username: </label> <!-- Label for people to input their username -->
+		<input type="text" name="username" />
+	</div>
+
+	<div>
+		<label for="password">Password: </label> <!-- Label for people to input their password -->
+		<input type="password" name="password" />
+	</div>
+
+	<div>
+		<button type="submit" class="btn btn-primary" data-toggle="button" aria-pressed="false" autocomplete="off"> Submit </button> <!-- submits the information on the page -->
+	</div>
+</form>
