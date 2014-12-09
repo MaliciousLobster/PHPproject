73ff0de5e5d19e8a44f6545138e69f3c6eb107bd
NateM <Mr.Awsmness@gmail.com>
2014-12-09 10:59:55 -0800

    video 44: Created a salt for the password

--- controller/create-user.php
+++ controller/create-user.php
@@ -5,8 +5,21 @@
 	$username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_STRING); //filters input that is passed to the file, FILTER_SANITIZE_STRING makes the value display as a string
 	$password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_STRING); //filters input that is passed to the file, FILTER_SANITIZE_STRING makes the value display as a string
 
-	echo $password;
+	$salt = "$5$" . "rounds=5000$" . uniqid(mt_rand(), true) . "$"; //creating a salt variable, running the password ecrypter SHA_256 5000 times, then creating a unique id and making sure it is random
 
-	$salt = "$5$" . "rounds=5000$" . uniqid(mt_rand(), true) . "$"; //creating a salt variable, running a password encrypter 5000 times, then creating a unique id and making sure it is random
+	$hashedPassword = crypt($password, $salt); //stores the result of the function crypt which takes the values of password and salt and encrypts it
 
-	echo $salt;	
+
+	//creating a query that inserts into the table Users, sets values to email, username, password, and salt
+	$query = $_SESSION["connection"]->query("INSERT INTO users SET "
+		. "email = '$email',"
+		. "username = '$username',"
+		. "password = '$hashedPassword',"
+		. "salt = '$salt'");
+
+	if($query) {
+		echo "Successfully created user: $username";
+	}
+	else {
+		echo "<p>" . $_SESSION["connection"]->error . "</p>";
+	}
